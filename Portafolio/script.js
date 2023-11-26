36ca412a2c56406252a9fff70358fcdf138463e7
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-11-25 23:31:37 -0600

    update

--- Portafolio/script.js
+++ Portafolio/script.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuLinks = document.querySelectorAll("nav a");
+  const toggleButton = document.getElementById("toggle-button"); // Agrega un botón de alternancia en tu HTML
+
+  toggleButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+    var currentTheme = toggleButton.innerHTML;
+
+    if (currentTheme.includes("bi-sun")) {
+      toggleButton.innerHTML = '<i class="bi bi-moon"></i>';
+    } else {
+      toggleButton.innerHTML = '<i class="bi bi-sun"></i>';
+    }
+    // Agrega más elementos que deben cambiar de estilo en el modo oscuro
+  });
 
   menuLinks.forEach((link) => {
-    link.addEventListener('click',function(e){
-        e.preventDefault();
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-        targetSection.scrollIntoView({behavior:'smooth'});
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetSection = document.getElementById(targetId);
+      targetSection.scrollIntoView({ behavior: "smooth" });
     });
   });
 });
