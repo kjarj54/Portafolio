e434cf4cfdc4a81f97507820f17828df0ca6514a
kjarj54 <kjarj54@gmail.com>
2025-07-27 11:16:06 -0600

    [UPDATE DEPENDENCIES AND ROMVE UNUSED]: update dependencies and restructure styles
    - Updated Astro to version 5.12.3 and Tailwind CSS to version 4.1.11.
    - Removed unused dependencies: @astrojs/react, @astrojs/tailwind, @types/react, @types/react-dom, react, and react-dom.
    - Added global CSS import in Layout.astro for Tailwind CSS.
    - Deleted outdated Tailwind configuration file.
    - Created a new global CSS file to import Tailwind CSS styles.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwind from '@astrojs/tailwind';
-
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [],
   site: 'https://arauzdev.netlify.app',
   output: 'static',
   compressHTML: true,
