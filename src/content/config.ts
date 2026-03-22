14749218bd61b3b2845ed3753f22719f5a5f2ead
kjarj54 <kjarj54@gmail.com>
2026-03-22 16:04:41 -0600

    [FEAT]: Add SEO component, blog post layout, and sitemap integration

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod'
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishedDate: z.coerce.date(),
+    image: z.string().optional(),
+    tags: z.array(z.string()).default([]),
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collections = { blog };
