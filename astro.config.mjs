319066b1f473ab88c8bacde98781f2d15b51508e
kjarj54 <kjarj54@gmail.com>
2026-03-25 20:28:29 -0600

    fix: update site URL in astro.config.mjs to point to the correct deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.astrojs.com',
+  site: 'https://arauzdev.netlify.app/',
   integrations: [sitemap()],
   output: 'server',
   adapter: netlify()
